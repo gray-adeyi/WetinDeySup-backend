071e8bfd6be2772a917bf0ef7ce01047848afff2
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-18 11:05:44 +0100

    Fix typo in startup event

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@ from fastapi.responses import RedirectResponse
 from app.db import initialize_database
 
 app = FastAPI()
-@app.on_event('starup')
+
+
+@app.on_event("startup")
 async def on_startup():
     await initialize_database()
+
 
 @app.get("/")
 async def redirect_to_docs():
