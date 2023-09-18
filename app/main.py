24711a6378ad1a7f638c5d24f98b1b2f6912b7da
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-18 11:02:10 +0100

    Implement init database on app startup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from app.db import initialize_database
+
 app = FastAPI()
+@app.on_event('starup')
+async def on_startup():
+    await initialize_database()
 
 @app.get("/")
 async def redirect_to_docs():
