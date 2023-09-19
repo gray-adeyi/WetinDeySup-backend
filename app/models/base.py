c242ea8b3958888ac4b00465dcff2628ef14eb0e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-19 17:28:10 +0100

    Flag model id

--- app/models/base.py
+++ app/models/base.py
@@ -5,4 +5,5 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
+    # TODO: default_factory=uuid.uuid4 is raising errors
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, type_=Uuid)
